1835e046e7000b0fee1dc5f5152d664251340dff
SJ <100001092+bnbnac@users.noreply.github.com>
2024-07-29 12:49:52 +0900

    history

--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router";
-import { AuthProvider, useAuth } from "./tools/AuthContext";
-import useSetupInterceptors from "./tools/useSetupInterceptors";
-
-const App = () => {
-  useSetupInterceptors();
-
-  return <RouterProvider router={router} />;
-};
+import { AuthProvider } from "./tools/AuthContext";
+import History from "./tools/History";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={router} History={History}></RouterProvider>
     </AuthProvider>
   </React.StrictMode>
 );
--- src/tools/Api.js
+++ src/tools/Api.js
@@ -1,8 +1,53 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../tools/AuthContext";
 
 const Api = axios.create({
   baseURL: process.env.REACT_APP_WEB_SERVER,
   withCredentials: true,
 });
 
+const refreshAccessToken = async () => {
+  try {
+    const response = await Api.post("/auth/refresh");
+    console.log("Refresh token successful", response.data);
+  } catch (error) {
+    console.error("Refresh token request failed:", error);
+    throw error;
+  }
+};
+
+export const setupInterceptors = (navigate, logout) => {
+  Api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      console.log("Interceptor hit", error);
+      const originalRequest = error.config;
+
+      if (error.response && error.response.status === 401) {
+        if (!originalRequest.url.includes("/auth/refresh")) {
+          console.log("401 error detected");
+          if (!originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+              await refreshAccessToken();
+              return Api(originalRequest);
+            } catch (refreshError) {
+              console.log("ref err");
+              history.push("/login");
+            }
+          }
+        } else {
+          console.log("else");
+          history.push("/login");
+        }
+      }
+      console.log("fin");
+      history.push("/login");
+      return Promise.reject(error);
+    }
+  );
+};
+
+setupInterceptors();
 export default Api;
