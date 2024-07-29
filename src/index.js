1148bd4970d1551faca5ccaa8fa9b186ba817ae5
SJ <100001092+bnbnac@users.noreply.github.com>
2024-07-29 13:01:24 +0900

    history

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
 import router from "./Router";
 import { AuthProvider } from "./tools/AuthContext";
-import History from "./tools/History";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} History={History}></RouterProvider>
+      <HistoryRouter>
+        <RouterProvider router={router}></RouterProvider>
+      </HistoryRouter>
     </AuthProvider>
   </React.StrictMode>
 );
